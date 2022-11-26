10a95db84ffba515a133af341a932da0c6e04fc1
Luketedwards <Luketedwards96@hotmail.com>
2022-11-26 16:22:55 +0000

    made responsive

--- flashcards/src/App.js
+++ flashcards/src/App.js
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, {
+  useState,
+  useEffect
+} from "react";
 import FlashcardList from "./FlashcardList";
 import './app.css'
 import axios from 'axios'
@@ -9,29 +12,38 @@ function App() {
 
   useEffect(() => {
     axios
-    .get('https://opentdb.com/api.php?amount=10')
-    .then(res => {
-      setFlashcards(res.data.results.map((questionItem, index) => {
-        const answer = questionItem.correct_answer
-        const options = [...questionItem.incorrect_answers, answer]
-        return {
-          id: `${index}-${Date.now()}`,
-          question: questionItem.question,
-          answer: answer,
-          options: options.sort(() => Math.random() - .5)
-        }
-      }))
-      console.log(res.data);
-    })
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+        console.log(res.data);
+      })
   }, [])
 
-  return (
-    <FlashcardList flashcards={flashcards}/> 
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  return ( <div className="container"> <
+    FlashcardList flashcards = {
+      flashcards
+    }
+    /> </div>
   );
 }
 
-const  SAMPLE_FLASHCARDS = [
-  {
+const SAMPLE_FLASHCARDS = [{
     id: 1,
     question: 'What is 2 + 2?',
     answer: '4',
@@ -53,6 +65,6 @@ const  SAMPLE_FLASHCARDS = [
       'Answer',
     ]
   },
-  
+
 ]
 export default App;
